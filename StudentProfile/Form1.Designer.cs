fd4285c49df589d4b02427bb98adf241acc2543f
Stephen De Jesus <stephendejesus171@gmail.com>
2026-09-12 19:33:55 +0800

    Update student profile output.

--- StudentProfile/Form1.Designer.cs
+++ StudentProfile/Form1.Designer.cs
@@ -37,9 +37,9 @@
 			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.label1.Location = new System.Drawing.Point(142, 181);
 			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(547, 39);
+			this.label1.Size = new System.Drawing.Size(587, 39);
 			this.label1.TabIndex = 0;
-			this.label1.Text = "Student Profile - Stephen De Jesus";
+			this.label1.Text = "Student Profile - GitHub Beginner Lab";
 			// 
 			// Form1
 			// 
