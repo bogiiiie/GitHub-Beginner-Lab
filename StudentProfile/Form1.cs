22746da882567e0535209a5c6d50773dfcd818db
Stephen De Jesus <stephendejesus171@gmail.com>
2026-09-12 23:41:50 +0800

    Implement update contact info (#2)

--- StudentProfile/Form1.cs
+++ StudentProfile/Form1.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace StudentProfile
@@ -15,6 +8,39 @@ namespace StudentProfile
 		public Form1()
 		{
 			InitializeComponent();
+		}
+
+		private void btnSubmit_Click(object sender, EventArgs e)
+		{
+			string email = txtEmailAddress.Text.Trim();
+
+			if (IsValidEmail(email))
+			{
+				MessageBox.Show("Profile updated successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+			}
+			else
+			{
+				MessageBox.Show("Please enter a valid email address (e.g., name@domain.com).", "Invalid Email", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				txtEmailAddress.Focus();
+				txtEmailAddress.SelectAll();
+			}
+		}
+
+		private bool IsValidEmail(string email)
+		{
+			if (string.IsNullOrWhiteSpace(email))
+				return false;
+
+			try
+			{
+				return System.Text.RegularExpressions.Regex.IsMatch(email,
+					@"^[^@\s]+@[^@\s]+\.[^@\s]+$",
+					System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+			}
+			catch
+			{
+				return false;
+			}
 		}
 	}
 }
